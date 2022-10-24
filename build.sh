08e0bf570294ad1dccd319047e164044ffc7bbde
Sean Callahan <seanahan5000@gmail.com>
2022-10-24 11:48:43 -0500

    File table support Module loading of elevator, alien.id, and diagnoser Disk side message support Fixes to hall filtering and general dispatch Fixes to elevator dispatch integration

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ fi
 # Assemble all source files
 #---------------------------------------
 
-$ASM NDOS   NDOS.525    -lst $OBJ/NDOS.525.LST
+$ASM NDOS   NDOS.525    -lst $OBJ/NDOS.525.LST -ent $SRC/NDOS/EXT.S
 $ASM COMMON ASM.COMMON  -lst $OBJ/COMMON.LST   -ent $SRC/COMMON/EXT.S
 $ASM HALLS  ASM.HALLS   -lst $OBJ/HALLS.LST    -ent $SRC/HALLS/EXT.S
 $ASM CAMP   ASM.CAMP    -lst $OBJ/CAMP.LST     -ent $SRC/CAMP/EXT.S
@@ -31,20 +31,27 @@ $ASM ALIENS ASM.PICS    -lst $OBJ/PICS.LST
 $ASM ALIENS ALIEN.DESC.12
 $ASM ALIENS ALIEN.DESC.345
 
-$ASM TEST   GAME.STATE.S
-$ASM TEST   TEST.CONTROL.S -lst $OBJ/CONTROL.LST
+$ASM TEST   GAME.STATE
+$ASM TEST   TEST.CONTROL -lst $OBJ/CONTROL.LST
 
 #---------------------------------------
 # Build disk .nib images
 #---------------------------------------
 
 A2NIB="$TOOLS/a2nib -disk $DSK/naja2.nib"
-$A2NIB -create -volume 0
+$A2NIB -create -volume 1
 
-$A2NIB $OBJ/ALIEN.PICS.12   -t 00 -s 00
-$A2NIB $OBJ/ALIEN.PICS.345  -t 10 -s 00
-$A2NIB $OBJ/ALIEN.DESC.12   -t 20 -s 00
-$A2NIB $OBJ/ALIEN.DESC.345  -t 21 -s 00
+
+$A2NIB $OBJ/ALIEN.PICS.12   -t 00 -s 00   # A900
+$A2NIB $OBJ/ALIEN.PICS.345  -t 0B -s 00   # C000
+$A2NIB $OBJ/ALIEN.DESC.12   -t 17 -s 00   # 0E00
+$A2NIB $OBJ/ALIEN.DESC.345  -t 18 -s 00   # 0D00
+
+$A2NIB $OBJ/DIAGNOSE        -t 19 -s 00   # 14 sectors
+                                          #  2 sectors
+$A2NIB $OBJ/ALIEN.ID        -t 1A -s 00   #  6 sectors
+                                          #  2 sectors
+$A2NIB $OBJ/ELEVATOR        -t 1A -s 08   #  8 sectors
 
 #---------------------------------------
 # Copy all project files
